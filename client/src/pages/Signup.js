cdd8f2cb70d979e8b5059f186715c8c250038bad
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-22 13:20:07 -0500

    changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 export default function Signup() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function signUp(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/signup', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+    }
+
+
     return (
-        <form className="signup">
+        <form className="signup" onSubmit={signUp}>
         <h1>Sign Up</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Username" value={username} onChange={ev => setUsername(ev.target.value)} />
+        <input type="password" placeholder="Password" value={password}onChange={ev => setPassword(ev.target.value)}  />
         <button>Register</button>
     </form>
     );
