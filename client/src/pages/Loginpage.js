752efa10d5fe65a38280251b3dc85bb9972ecd21
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-22 15:29:48 -0500

    working code

--- client/src/pages/Loginpage.js
+++ client/src/pages/Loginpage.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 
 export default function Loginpage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function login(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+    }
+        
     return (
     
-            <form className='login'>
+            <form className='login'onSubmit={login}>
                 <h1>Login</h1>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
+                <input type="text"
+                 placeholder="Username" 
+                 value = {username} 
+                 onChange = {ev => setUsername(ev.target.value)} />
+                <input type="password"
+                 placeholder="Password"
+                 value = {password}
+                  onChange = {ev => setPassword(ev.target.value)} />
                 <button>Login</button>
             </form>
         
