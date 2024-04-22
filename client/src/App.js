e605aee5160dff0dde061be5e9f96585c5ba4936
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-22 10:46:53 -0500

    structured App.js and styles css

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,48 @@ function App() {
         <nav>
           <a href=''>Login</a>
           <a href=''>Sign Up</a>
+          <a href=''>About Us</a>
         </nav>
       </header>
       < div className='hero'>
         <div className="image">
-      <img src = 'https://www.cafonline.org/images/default-source/cafcdoimages/ai-blog-post-summary.png?sfvrsn=70a15647_0' alt = 'hero' />
-      </div>
-      <div className='text'>
-      <h2>Helping Charities</h2>
-      <p>At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work. Founded in 2024, our platform was born from a desire to bridge the gap between charities in need and potential donors. Utilizing the latest technologies in the MERN stack—MongoDB, Express.js, React, and Node.js—we have crafted a seamless, single-page application that prioritizes user experience and responsiveness. Each charity on our platform is vetted and provided with a comprehensive profile that includes mission statements, impact scores, and direct donation capabilities. Our innovative platform supports real-time interactions and transactions, including one-time or recurring donations through Stripe. This ensures that every contribution directly supports your chosen cause in a secure and transparent manner. We also incorporate GraphQL to efficiently manage data transactions, making your experience as smooth and reliable as possible.
+          <img src='https://www.cafonline.org/images/default-source/cafcdoimages/ai-blog-post-summary.png?sfvrsn=70a15647_0' alt='hero' />
+        </div>
+        <div className='text'>
+          <h2>Helping Charities</h2>
+          <p className='info'>
+            <a cLassName="author">By John Doe </a>
+            <time>2024-04-22</time>
+          </p>
+          <p className="summary">At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work.
           </p>
         </div>
       </div>
       < div className='hero'>
         <div className="image">
-
-      <img src = 'https://www.cafonline.org/images/default-source/cafcdoimages/ai-blog-post-summary.png?sfvrsn=70a15647_0' alt = 'hero' />
+          <img src='https://www.cafonline.org/images/default-source/cafcdoimages/ai-blog-post-summary.png?sfvrsn=70a15647_0' alt='hero' />
         </div>
-      <div className='text'>
-      <h2>Helping Charities</h2>
-      <p>At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work. Founded in 2024, our platform was born from a desire to bridge the gap between charities in need and potential donors. Utilizing the latest technologies in the MERN stack—MongoDB, Express.js, React, and Node.js—we have crafted a seamless, single-page application that prioritizes user experience and responsiveness. Each charity on our platform is vetted and provided with a comprehensive profile that includes mission statements, impact scores, and direct donation capabilities. Our innovative platform supports real-time interactions and transactions, including one-time or recurring donations through Stripe. This ensures that every contribution directly supports your chosen cause in a secure and transparent manner. We also incorporate GraphQL to efficiently manage data transactions, making your experience as smooth and reliable as possible.
+        <div className='text'>
+          <h2>Helping Charities</h2>
+          <p className='info'>
+            <a cLassName="author">By John Doe </a>
+            <time>2024-04-22</time>
+          </p>
+          <p className="summary">At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work.
           </p>
         </div>
       </div>
       < div className='hero'>
         <div className="image">
-      <img src = 'https://www.cafonline.org/images/default-source/cafcdoimages/ai-blog-post-summary.png?sfvrsn=70a15647_0' alt = 'hero' />
-      </div>
-      <div className='text'>
-      <h2>Helping Charities</h2>
-      <p>At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work. Founded in 2024, our platform was born from a desire to bridge the gap between charities in need and potential donors. Utilizing the latest technologies in the MERN stack—MongoDB, Express.js, React, and Node.js—we have crafted a seamless, single-page application that prioritizes user experience and responsiveness. Each charity on our platform is vetted and provided with a comprehensive profile that includes mission statements, impact scores, and direct donation capabilities. Our innovative platform supports real-time interactions and transactions, including one-time or recurring donations through Stripe. This ensures that every contribution directly supports your chosen cause in a secure and transparent manner. We also incorporate GraphQL to efficiently manage data transactions, making your experience as smooth and reliable as possible.
+          <img src='https://www.cafonline.org/images/default-source/cafcdoimages/one-planet-image.png?sfvrsn=60814847_0' alt='hero' />
+        </div>
+        <div className='text'>
+          <h2>Helping Charities</h2>
+          <p className='info'>
+            <a cLassName = 'author'>By John Doe </a>
+            <time>2024-04-22</time>
+          </p>
+          <p className="summary">At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work.
           </p>
         </div>
       </div>
