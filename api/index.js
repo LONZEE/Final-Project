876c0194a3ba423273d69b0f1bb0d2ae8dec0093
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-22 13:52:12 -0500

    connecting o mongoose

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://LONZEE:YKO2YsVqA1sYHqRE@charity.zdmunij.mongodb.net/?retryWrites=true&w=majority&appName=Charity');  
 
 app.post('/signup', (req,res) => {
     const {username, password} = req.body;
@@ -12,3 +17,5 @@ app.post('/signup', (req,res) => {
 });
 
 app.listen(4000);
+//mongodb+srv://LONZEE:YKO2YsVqA1sYHqRE@charity.zdmunij.mongodb.net/
+//YKO2YsVqA1sYHqRE
--- client/src/hero.js
+++ client/src/hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
         <div className='text'>
           <h2>Helping Charities</h2>
           <p className='info'>
-            <a cLassName="author">By John Doe </a>
+            <a className="author">By John Doe </a>
             <time>2024-04-22</time>
           </p>
           <p className="summary">At Charity Donation Platform, we are dedicated to creating connections that matter. Our mission is to empower charitable organizations by providing them with a cutting-edge platform where they can showcase their causes, connect with donors, and receive vital funds to continue their impactful work.
